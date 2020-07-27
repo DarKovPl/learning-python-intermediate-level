fc51ed8897b14817ad49a4fe4343aca0817d0cca
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-07-27 22:00:14 +0200

    One file was modified: 09_AutomaticConversionToTheLogicalType

--- 09_AutomaticConversionToTheLogicalType.py
+++ 09_AutomaticConversionToTheLogicalType.py
@@ -71,3 +71,24 @@ if len(list_of_errors) > 0:
 print('-------------------------------------------------------------------')
 
 # Lab
+
+option_list = [' load data', ' export data', ' analyze & predict']
+chosen_option = 0
+
+
+while not chosen_option:
+
+    try:
+        chosen_option = int(input('Please choose one of the options: \n{}.\nEnter the number of your choice here -->'
+                                  .format('\n'.join(str(num + 1) + elem
+                                                    for num, elem in zip(range(len(option_list)), option_list)))))
+        if 0 < chosen_option <= 3:
+            print('You chosen: {0:d}{1:s}'.format(chosen_option, option_list[chosen_option - 1]))
+
+        else:
+            print('Your entered value is above the range: %d' % chosen_option if chosen_option > 3
+                  else 'Your entered value is below the range: %d' % chosen_option)
+            chosen_option = 0
+
+    except ValueError as e:
+        print("\nThis error has occurred: '%s'" % e, 'Please enter the correct int value.')

8fe2f61384ba8219cd0e44c87267a35e5e8fba56
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-08-19 22:02:46 +0200

    One file was modified: 54_FunctionAsArgumentInFunction

--- 54_FunctionAsArgumentInFunction.py
+++ 54_FunctionAsArgumentInFunction.py
@@ -27,6 +27,38 @@ print('--------------')
 
 for activity, obj in cookbook:
     cook(activity, obj)
+print('-------------------------------------------------------------------')
 
 # Lab
 
+x_table = list(range(11))
+
+
+def double(x):
+    return 2 * x
+
+
+def root(x):
+    return x ** 2
+
+
+def negative(x):
+    return -x
+
+
+def div2(x):
+    return x / 2
+
+
+def generate_values(function, ingredients):
+    """This function is a common interface for the above functions, and it is able to send
+     ingredients from x_table to every function and download the result of the calculation and print it"""
+
+    for x_ing in ingredients:
+        print("Function: '{}' and the result of calculation: {}".format(function.__name__, function(x_ing)))
+
+
+generate_values(double, x_table)
+generate_values(root, x_table)
+generate_values(negative, x_table)
+generate_values(div2, x_table)

4ba2001efb1dddecef078d192539e456439e1302
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-08-24 21:09:07 +0200

    One file was modified: 57_FunctionIsReturningFunction

--- 57_FunctionIsReturningFunction.py
+++ 57_FunctionIsReturningFunction.py
@@ -38,28 +38,28 @@ print('-------------------------------------------------------------------')
 units = {'minutes': 60, 'hours': 3600, 'days': 86400}
 
 
-def main_function(enter_date=input("Please enter here the date 'RRRR-MM-DD HH MM SS': "),
-                  units_of_time=input('Please chose which unit of date you want to work?\n{}\n'
-                                      'Enter chosen value here-->'
-                                      ''.format('\n'.join(unit + '-' + str(val) for unit, val in units.items())))):
+def main_function(enter_date):
+
     f_minutes_variable = """
 from dateutil import parser
 from datetime import datetime
 
 
-def f_time(input_date=parser.parse(str({})),
-           current_time=datetime.now(), unit={}):
-
+def f_time(units_of_time, input_date=parser.parse('{}'),
+           current_time=datetime.now()):
+    
     result = current_time - input_date
     seconds = result.total_seconds()
-    output =  round(round(seconds, 0) / unit, 0)
+    output =  round(round(seconds, 0) / int(units_of_time), 0)
 
     return output
-""".format(enter_date, units_of_time)
+""".format(enter_date)
+
     exec(f_minutes_variable, globals())
 
-    return f_time()
+    return f_time
 
 
-print(main_function())
-
+rt = main_function(enter_date=input("Please enter here the date 'RRRR-MM-DD HH MM SS': "))
+print(rt(units_of_time=input('Please chose which unit of date you want to work?\n{}\nEnter chosen value here-->'
+                             ''.format('\n'.join(unit + '-' + str(val) for unit, val in units.items())))))

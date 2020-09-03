4c0415698e65ff7c534351be4b3599ed809379b6
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-09-03 22:03:14 +0200

    One file was modified: 60_WrapperForFunction

--- 60_WrapperForFunction.py
+++ 60_WrapperForFunction.py
@@ -1,65 +1,97 @@
 import datetime
 import functools
+import time
+
+# def change_salary(emp_name, new_salary, is_bonus=False):
+#     print("Changing salary for {} to {} as bonus={}".format(emp_name,
+#                                                             new_salary,
+#                                                             is_bonus))
+#     return new_salary
+#
+#
+# print(change_salary('Johnson', 20000, True))
+# print('--------------')
+#
+#
+# def create_function_with_wrapper(func):
+#     def _func_with_wrapper(*args, **kwargs):
+#         print("Function '{}' started at {}".format(func.__name__,
+#                                                    datetime.datetime.now().isoformat()))
+#         print('Following arguments were used:')
+#         print(args, kwargs)
+#         result = func(*args, **kwargs)
+#         print('Function returned {}'.format(result))
+#         return result
+#
+#     return _func_with_wrapper
+#
+#
+# change_salary_with_log = create_function_with_wrapper(change_salary)
+#
+# print(change_salary_with_log('Johnson', 30000, is_bonus=True))
+# print('-------------------------------------------------------------------')
+#
+# #       Second way:
+# print('Second way\n')
+#
+# change_salary = create_function_with_wrapper(change_salary)
+#
+# print(change_salary('Johnson', 30000, is_bonus=True))
+# print('-------------------------------------------------------------------')
+#
+# #       Third way
+# print('Third way\n')
+#
+#
+# def create_function_with_wrapper(func):
+#     def _func_with_wrapper(*args, **kwargs):
+#         print("Function '{}' started at {}".format(func.__name__,
+#                                                    datetime.datetime.now().isoformat()))
+#         print('Following arguments were used:')
+#         print(args, kwargs)
+#         result = func(*args, **kwargs)
+#         print('Function returned {}'.format(result))
+#         return result
+#
+#     return _func_with_wrapper
+#
+#
+# @create_function_with_wrapper
+# def change_salary(emp_name, new_salary, is_bonus=False):
+#     print("Changing salary for {} to {} as bonus={}".format(emp_name,
+#                                                             new_salary,
+#                                                             is_bonus))
+#     return new_salary
+#
+#
+# print(change_salary('Betty', 45000, is_bonus=True))
+
+#  Lab
 
 
-def change_salary(emp_name, new_salary, is_bonus=False):
-    print("Changing salary for {} to {} as bonus={}".format(emp_name,
-                                                            new_salary,
-                                                            is_bonus))
-    return new_salary
+def get_sequence_wrapper(function):
+    def _wrapped_function(*args):
+
+        time_start = time.time()
+        result = function(*args)
+        time_stop = time.time()
+        counted_time = time_stop - time_start
+
+        print(counted_time)
+
+        return counted_time
+    return _wrapped_function
 
 
-print(change_salary('Johnson', 20000, True))
-print('--------------')
+@get_sequence_wrapper
+def get_sequence(n):
+    if n <= 0:
+        return 1
+    else:
+        v = 0
+        for i in range(n):
+            v += 1 + (get_sequence(i - 1) + get_sequence(i)) / 2
+        return v
 
 
-def create_function_with_wrapper(func):
-    def _func_with_wrapper(*args, **kwargs):
-        print("Function '{}' started at {}".format(func.__name__,
-                                                   datetime.datetime.now().isoformat()))
-        print('Following arguments were used:')
-        print(args, kwargs)
-        result = func(*args, **kwargs)
-        print('Function returned {}'.format(result))
-        return result
-
-    return _func_with_wrapper
-
-
-change_salary_with_log = create_function_with_wrapper(change_salary)
-
-print(change_salary_with_log('Johnson', 30000, is_bonus=True))
-print('-------------------------------------------------------------------')
-
-#       Second way:
-
-change_salary = create_function_with_wrapper(change_salary)
-
-print(change_salary('Johnson', 30000, is_bonus=True))
-print('-------------------------------------------------------------------')
-
-
-#       Third way
-
-def create_function_with_wrapper(func):
-    def _func_with_wrapper(*args, **kwargs):
-        print("Function '{}' started at {}".format(func.__name__,
-                                                   datetime.datetime.now().isoformat()))
-        print('Following arguments were used:')
-        print(args, kwargs)
-        result = func(*args, **kwargs)
-        print('Function returned {}'.format(result))
-        return result
-
-    return _func_with_wrapper
-
-
-@create_function_with_wrapper
-def change_salary(emp_name, new_salary, is_bonus=False):
-    print("Changing salary for {} to {} as bonus={}".format(emp_name,
-                                                            new_salary,
-                                                            is_bonus))
-    return new_salary
-
-
-print(change_salary('Betty', 45000, is_bonus=True))
+print(get_sequence(12))

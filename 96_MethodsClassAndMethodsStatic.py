e0d796d5d158b3c72fb37b4002160c4717efc12a
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-12-26 21:12:07 +0100

    One file was added: 96_MethodsClassAndMethodsStatic

--- 96_MethodsClassAndMethodsStatic.py
+++ 96_MethodsClassAndMethodsStatic.py
@@ -1,3 +1,6 @@
+import pickle
+import glob
+
 brandOnSale = 'Opel'
 
 
@@ -65,3 +68,102 @@ print('-------------------------------------------------------------------')
 
 print(car_03.read_from_text(line_of_text))
 print(car_03.convert_KW_KM(50))
+print('-------------------------------------------------------------------')
+
+
+# Lab
+
+class Cake:
+    known_types = ['cake', 'muffin', 'meringue', 'biscuit', 'eclair',
+                   'christmas', 'pretzel', 'other']
+
+    bakery_offer = []
+
+    def __init__(self, name, kind, taste, additives, filling, wage, gluten_free=False, text=''):
+        self.name = name
+        if str(kind).lower() in self.known_types:
+            self.kind = kind
+        else:
+            self.kind = Cake.known_types[-1]
+        self.taste = taste
+        self.additives = additives
+        self.filling = filling
+        self.wage = wage
+        self.__gluten_free = gluten_free
+        if self.kind == 'Cake' or text == '':
+            self.__text = text
+        else:
+            self.__text = "This confectionery is not a cake, and we can't add any birthday wishes"
+        Cake.bakery_offer.append(self)
+
+    def show_info(self):
+        print(f'{self.name}'.upper())
+        print('Kind:%21s' % self.kind)
+        print('Taste:%20s' % self.taste)
+        if self.additives > []:
+            print(f"Additives:\n\t{'; '.join([str(add).capitalize() for add in self.additives])}")
+        if self.filling > '':
+            print(f'Filling:\n\t{self.filling}')
+        print(f'Wage: {self.wage}')
+        print('This product is gluten free' if self.__gluten_free else 'This product has gluten')
+        print(f'Text on the Cake: {self.text_properties}')
+
+    def set_filling(self, add_filling):
+        self.filling = add_filling
+
+    def add_additives(self, add_more_additives):
+        self.additives += add_more_additives
+
+    def __get_text(self):
+        return self.__text
+
+    def __set_text(self, new_text):
+        self.__text = new_text if self.kind == 'Cake' else "This confectionery is not a cake," \
+                                                           " and we can't add any birthday wishes"
+
+    def save_to_file(self, path):
+        pickle.dump(self, open(path, 'bw'))
+
+    @classmethod
+    def read_from_file(cls, path):
+        new_pie = pickle.load(open(path, 'rb'))
+        cls.bakery_offer.append(new_pie)
+        return new_pie
+
+    @staticmethod
+    def get_bakery_files(catalog_name):
+        print(glob.glob(f'{catalog_name}*.bakery'))
+
+    text_properties = property(__get_text, __set_text, None, '''Some documentation about this property''')
+
+
+cake_01 = Cake('Pear-Ginger Pie', 'Cake', 'Ginger',
+               additives=['pears', 'crystallized ginger',
+                          'orange juice', 'cinnamon'], filling='', wage=1.8)
+
+cake_02 = Cake('Italian Rainbow', 'Biscuit', 'Raspberry jam', additives=[],
+               filling='Layers of almond and coated in chocolate', wage=0.5, text='This is not a cake')
+
+cake_03 = Cake('Banana Cream', 'Cake', 'Almond-Banana Cream',
+               additives=['Almond cream', 'bananas',
+                          'whipping cream'], filling='Covered whipping cream', wage=0.9, gluten_free=True)
+
+cake_04 = Cake('Cocoa waffle', 'Waffle', 'Cocoa', [], 'Cocoa', 1.2)
+
+cake_01.set_filling('Stuffed with pears')
+cake_02.add_additives(['Almonds', 'raspberry jam', 'semisweet chocolate'])
+
+cake_03.text_properties = 'Happy Birthday !!!!!!!!!!!!!!!!'
+cake_04.text_properties = 'Enjoy'
+
+cake_01.save_to_file(f'/home/darek/Projects/to_task_96/pickle_file_{cake_01.name}.bakery')
+cake_02.save_to_file(f'/home/darek/Projects/to_task_96/pickle_file_{cake_02.name}.bakery')
+cake_05 = Cake.read_from_file(f'/home/darek/Projects/to_task_96/pickle_file_{cake_02.name}.bakery')
+
+print("Today's offer is:")
+for number, elem in enumerate(Cake.bakery_offer):
+    print('*' * 70)
+    print(f'Number:{number + 1}')
+    elem.show_info()
+print('------------------------------------------------------------')
+Cake.get_bakery_files('/home/darek/Projects/to_task_96/')

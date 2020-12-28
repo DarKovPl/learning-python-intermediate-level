008e8e332e88ca5596efe7c23d6705c5be42cfbc
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-12-28 21:00:37 +0100

    One file was modified: 96_MethodsClassAndMethodsStatic and One file was added: 99_ClassPropertiesWithDecorators

--- 99_ClassPropertiesWithDecorators.py
+++ 99_ClassPropertiesWithDecorators.py
@@ -1,74 +1,48 @@
-import pickle
 import glob
-
-brandOnSale = 'Opel'
-
-
-class Car:
-    number_of_cars = 0
-    list_of_cars = []
-
-    def __init__(self, brand, model, is_airbag_ok, is_painting_ok, is_mechanic_ok, is_on_sale):
-        self.brand = brand
-        self.model = model
-        self.is_airbag_OK = is_airbag_ok
-        self.is_painting_OK = is_painting_ok
-        self.is_mechanic_OK = is_mechanic_ok
-        self.__is_on_sale = is_on_sale
-
-        Car.number_of_cars += 1
-        Car.list_of_cars.append(self)
-
-    def is_damaged(self):
-        return not (self.is_airbag_OK and self.is_painting_OK and self.is_mechanic_OK)
-
-    def get_info(self):
-        print(f'{self.brand} {self.model}'.upper())
-        print(f'Air Bag -ok- {self.is_airbag_OK}')
-        print(f'Painting -ok- {self.is_painting_OK}')
-        print(f'Mechanic -ok- {self.is_mechanic_OK}')
-        print(f'Is on sale? {self.__is_on_sale}')
-        print('-' * 60)
-
-    def __get_is_on_sale(self):
-        return self.__is_on_sale
-
-    def __set_is_on_sale(self, new_is_on_sale_status):
-        if self.brand == brandOnSale:
-            self.__is_on_sale = new_is_on_sale_status
-            print(f'Changing status is_on_sale to {new_is_on_sale_status} for {self.brand}')
-        else:
-            print(f'Cannot change status is_on_sale. Sale is able only for: {brandOnSale}')
-
-    # we will write here the name of the variable by a big letter because we want to make this variable unique.
-    IS_ON_SALE = property(__get_is_on_sale, __set_is_on_sale, None, 'if set to true, the car is available in sale')
-
-    @classmethod
-    def read_from_text(cls, a_text):
-        a_new_car = cls(*a_text.split(':'))
-        return a_new_car
-
-    @staticmethod
-    def convert_KM_KW(KM):
-        return KM * 0.735
-
-    @staticmethod
-    def convert_KW_KM(KW):
-        return KW * 1.36
+import pickle
 
 
-line_of_text = 'Renault:Megane:True:True:False:False'
-car_03 = Car.read_from_text(line_of_text)
-car_03.get_info()
-print('-------------------------------------------------------------------')
-
-print('Converting 120 KM to KW', Car.convert_KM_KW(120))
-print('Converting 90 KW to KM', Car.convert_KW_KM(90))
-print('-------------------------------------------------------------------')
-
-print(car_03.read_from_text(line_of_text))
-print(car_03.convert_KW_KM(50))
-print('-------------------------------------------------------------------')
+# brandOnSale = 'Opel'
+#
+#
+# class Car:
+#
+#     def __init__(self, brand, model, is_airbag_ok, is_painting_ok, is_mechanic_ok, is_on_sale):
+#         self.brand = brand
+#         self.model = model
+#         self.is_airbag_OK = is_airbag_ok
+#         self.is_painting_OK = is_painting_ok
+#         self.is_mechanic_OK = is_mechanic_ok
+#         self.__is_on_sale = is_on_sale
+#
+#     @property
+#     def is_on_sale(self):
+#         return self.__is_on_sale
+#
+#     @is_on_sale.setter
+#     def is_on_sale(self, new_is_on_sale_status):
+#         if self.brand == brandOnSale:
+#             self.__is_on_sale = new_is_on_sale_status
+#             print(f'Changing status is_on_sale to {new_is_on_sale_status} for {self.brand}')
+#         else:
+#             print(f'Cannot change status is_on_sale. Sale is able only for: {brandOnSale}')
+#
+#     @is_on_sale.deleter
+#     def is_on_sale(self):
+#         self.__is_on_sale = None
+#
+#     @property
+#     def car_title(self):
+#         return f'Mark: {self.brand}  Model: {self.model}'.title()
+#
+#
+# car_01 = Car('Seat', 'Ibiza', True, True, True, False)
+#
+# print(car_01.is_on_sale)
+# car_01.is_on_sale = True
+# del car_01.is_on_sale
+# print(car_01.is_on_sale)
+# print(car_01.car_title)
 
 
 # Lab
@@ -108,7 +82,7 @@ class Cake:
             print(f'Filling:\n\t{self.filling}')
         print(f'Wage: {self.wage}')
         print('This product is gluten free' if self.__gluten_free else 'This product has gluten')
-        print(f'Text on the Cake: {self.text_properties}')
+        print(f'Text on the Cake: {self.set_text}')
 
     def set_filling(self, add_filling):
         self.filling = add_filling
@@ -116,10 +90,12 @@ class Cake:
     def add_additives(self, add_more_additives):
         self.additives += add_more_additives
 
-    def __get_text(self):
+    @property
+    def set_text(self):
         return self.__text
 
-    def __set_text(self, new_text):
+    @set_text.setter
+    def set_text(self, new_text):
         if self.kind == 'Cake':
             self.__text = new_text
 
@@ -128,15 +104,14 @@ class Cake:
 
     @classmethod
     def read_from_file(cls, path):
-        new_pie = pickle.load(open(path, 'rb'))
+        with open(path, 'br') as f:
+            new_pie = pickle.load(f)
         cls.bakery_offer.append(new_pie)
         return new_pie
 
     @staticmethod
     def get_bakery_files(catalog_name):
         print(glob.glob(f'{catalog_name}*.bakery'))
-
-    text_properties = property(__get_text, __set_text, None, '''Some documentation about this property''')
 
 
 cake_01 = Cake('Pear-Ginger Pie', 'Cake', 'Ginger',
@@ -163,14 +138,16 @@ for number, elem in enumerate(Cake.bakery_offer):
 print('------------------------------------------------------------')
 
 Cake.get_bakery_files('/home/darek/Projects/to_task_96/')
-print('----------------------------------------------------------------')
+print('------------------------------------------------------------')
 
-cake_03.text_properties = 'Happy Birthday !!!!!!!!!!!!!!!!'
-cake_04.text_properties = 'Enjoy'
+cake_03.set_text = 'Happy Birthday !!!!!!!!!!!!!!!!'
+cake_04.set_text = 'Enjoy'
 
 cake_01.save_to_file(f'/home/darek/Projects/to_task_96/pickle_file_{cake_01.name}.bakery')
 cake_02.save_to_file(f'/home/darek/Projects/to_task_96/pickle_file_{cake_02.name}.bakery')
 cake_05 = Cake.read_from_file(f'/home/darek/Projects/to_task_96/pickle_file_{cake_02.name}.bakery')
+cake_06 = Cake.read_from_file(f'/home/darek/Projects/to_task_96/pickle_file_{cake_01.name}.bakery')
+
 
 print("Today's offer is:")
 for number, elem in enumerate(Cake.bakery_offer):

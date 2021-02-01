fcaa6f6b2ccd4b385d52d5b6abb4a2f480589ba7
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2021-02-01 21:42:33 +0100

    One file was modified: 159_Itertools

--- 159_Itertools.py
+++ 159_Itertools.py
@@ -1,5 +1,5 @@
 import itertools as it
-import math
+import functools
 
 # my_list = ['a', 'b', 'c', 'd']
 #
@@ -41,15 +41,30 @@ import math
 
 notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
-perm = [note for note in it.permutations(notes, 4)]
+value_of_song_notes = 4  # Here you can change the number of song notes
 
-comb = [note for note in it.product(notes, notes, notes, notes)]
+var_without_rep = [note for note in it.permutations(notes, value_of_song_notes)]
+
+var_with_rep = [note for note in it.product(notes, repeat=value_of_song_notes)]
 
 factorial = lambda num: 1 if num <= 1 else num * factorial(num - 1)
 
-# power = lambda num: num **
+power = lambda num, power: num ** power
 
-# def check_and_answer(list_of_elements, comb_of_perm, comb_of_, factorial, ):
-#
-#
-#     if len(list_of_elements) ==
+
+def check_and_print(song_without_rep, song_with_rep, fact, pow, for_div):
+
+    math_form = fact / for_div
+    if (len(song_without_rep), len(song_with_rep)) == (math_form, pow):
+        print(f'Amount of the variations WITHOUT repetition is correct, and the total is {int(math_form)}.',
+              f'Amount of the variations WITH repetition is correct, and the total is {pow}')
+    else:
+        raise Exception('Something went wrong. Please check arguments')
+
+
+to_factorial = functools.partial(factorial, len(notes))
+to_power = functools.partial(power, len(notes), value_of_song_notes)
+to_div = functools.partial(factorial, len(notes) - value_of_song_notes)
+for_check_and_print = functools.partial(check_and_print, var_without_rep, var_with_rep)
+
+for_check_and_print(to_factorial(), to_power(), to_div())
